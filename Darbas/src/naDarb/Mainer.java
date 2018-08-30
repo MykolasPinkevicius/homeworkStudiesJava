32e93920504f6968b686b70cd868a32c2554be74
Mykolas Pinkevičius <mdpbeats@gmail.com>
2018-08-30 18:18:14 +0300

    Maven Project , The First Try

--- Darbas/src/naDarb/Mainer.java
+++ Darbas/src/naDarb/Mainer.java
@@ -9,6 +9,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * @
+ * @author mykolaspinkevicius
+ *
+ */
 public class Mainer {
 
 	public static void main(String[]args) {
---
+++ maven-demo/src/main/java/com/mykolaspinke/App.java
@@ -0,0 +1,16 @@
+package com.mykolaspinke;
+
+import java.util.Locale;
+
+import com.ibm.icu.text.RuleBasedNumberFormat;
+
+public class App {
+	public static void main (String[]args) {
+		Locale location = new Locale(args[0], args[1].toUpperCase());
+		RuleBasedNumberFormat formatter = new RuleBasedNumberFormat(location, RuleBasedNumberFormat.SPELLOUT);
+		
+		for	( int i = 1; i < args.length ; i++) {
+			System.out.print(formatter.format(Integer.parseInt(args[i])));
+		}
+	}
+}
